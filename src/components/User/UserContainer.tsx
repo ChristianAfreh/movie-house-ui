87b767b533953330b5bf2563364e3e436fd78b12
Christian Afreh <christian.afreh@primustechgh.net>
2023-12-08 17:42:17 +0000

    minor ui fixes on userContainer component

--- src/components/User/UserContainer.tsx
+++ src/components/User/UserContainer.tsx
@@ -1,15 +1,16 @@
 
 import UserAvatar from './UserAvatar';
-import { BsBell, BsLockFill, BsMoon } from 'react-icons/bs';
+import { BsBell, BsLockFill } from 'react-icons/bs';
+import { RiMoonClearFill } from 'react-icons/ri';
 import styles from '../../styles/UserContainer.module.css';
 
 export default function UserContainer() {
   return (
     <div className={styles.userContainerDiv} >
-        <button className="rounded-full bg-gray-200 p-4 text-gray-500 text-lg"><BsMoon /></button>
+        <button className="rounded-full bg-gray-200 p-4 text-gray-500 text-lg"><RiMoonClearFill /></button>
         <button className="rounded-full bg-gray-200 p-4 text-gray-500 text-lg"><BsBell /></button>
         <button className='rounded-full bg-gray-200 p-4 text-gray-500 text-lg'><BsLockFill /></button>
-        <UserAvatar />
+        <button><UserAvatar /></button>
         
     </div>
   )
